9b84047858351d7adde8e76b863b35caae6f062c
MathLike <benjamin.ubilla@uc.cl>
2023-05-18 11:11:46 -0400

    Actualizado

--- manim_course_utils/markdown_tex_template.py
+++ manim_course_utils/markdown_tex_template.py
@@ -6,5 +6,5 @@ __all__ = ["MarkdownTexTemplate"]
 
 class MarkdownTexTemplate(TexTemplate):
     def __init__(self):
-        super().__init__(tex_compiler="lualatex")
+        super().__init__(tex_compiler="lualatex", output_format=".pdf")
         self.add_to_preamble(r"\usepackage[fencedCode,hashEnumerators,hybrid,smartEllipses]{markdown}")
